8692412dbffe8b3d8c7a6263961fbdfd040e36f5
IvanSabljić <kalarhont@gmail.com>
2025-10-20 20:44:45 +0200

    Izmijenjen kod u petlji for: i <= 25

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
         int suma = 0;
-        for (int i = 1; i <= 15; i++) {
+        for (int i = 1; i <= 25; i++) {
             suma += i;
         }
 
