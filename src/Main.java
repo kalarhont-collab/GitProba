4b7513656a37b3d7ce54f8a5fdb8b1ed1c3146a2
IvanSabljić <kalarhont@gmail.com>
2025-10-20 20:21:57 +0200

    Ispis sume na kraju programa...

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello and welcome!");
-
+        int suma = 0;
         for (int i = 1; i <= 5; i++) {
+            suma += i;
+        }
 
-            System.out.println("i = " + i);
+            System.out.println("Suma brojeva je: " + suma);
         }
     }
-}
